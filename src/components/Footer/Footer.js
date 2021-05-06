9826105f6748540d991e019c49b463dd05ff9787
Yusuf Alp <alpyusuf007@gmail.com>
2021-05-06 12:47:36 -0700

    add the footer section for the links and the social media

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="group">
+        <div className="logo">
+          <h1>Shortly</h1>
+        </div>
+        <div>
+          <h3>Features</h3>
+          <p>Link Shortening</p>
+          <p>Branded Links</p>
+          <p>Analytics</p>
+        </div>
+        <div>
+          <h3>Resources</h3>
+          <p>Blog</p>
+          <p>Developers</p>
+          <p>Support</p>
+        </div>
+        <div>
+          <h3>Company</h3>
+          <p>About</p>
+          <p>Our Team</p>
+          <p>Careers</p>
+          <p>Contact</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
