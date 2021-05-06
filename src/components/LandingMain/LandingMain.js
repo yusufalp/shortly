a99082ee66d24553f6df30d9959fb41ec70770c6
Yusuf Alp <alpyusuf007@gmail.com>
2021-05-06 14:00:12 -0700

    add classnames for css design

--- src/components/LandingMain/LandingMain.js
+++ src/components/LandingMain/LandingMain.js
@@ -1,18 +1,21 @@
 import React from 'react';
+import './LandingMain.css';
 
 const LandingMain = () => {
   return (
-    <div>
-      <div>
-        <h1>More than just shorter links</h1>
-        <p>
-          Build your brand’s recognition and get detailed insights
-          on how your links are performing.
+    <div className="landing-all">
+      <div className="landing-group">
+        <div>
+          <h1>More than just shorter links</h1>
+          <p>
+            Build your brand’s recognition and get detailed insights
+            on how your links are performing.
         </p>
-        <button type="submit">Get Started</button>
-      </div>
-      <div>
-        <img src="/images/illustration-working.svg" alt="person working on a desk" />
+          <button type="submit">Get Started</button>
+        </div>
+        <div>
+          <img className="landing-main-image" src="/images/illustration-working.svg" alt="person working on a desk" />
+        </div>
       </div>
       <div>
         <input type="text" name="shorten" id="shorten" placeholder="Shorten a link here..." />
