76683e7bc2d767eb4a67b5c6019594cec2eeb29a
Yusuf Alp <alpyusuf007@gmail.com>
2021-05-06 12:44:42 -0700

    create a call to action section at the bottom of the page

---
+++ src/components/CallToAction/CallToAction.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './CallToAction.css';
+
+const CallToAction = () => {
+  return (
+    <div className="call-to-action-main">
+      <h2>Boost your links today</h2>
+      <button type="submit">Get Started</button>
+    </div>
+  );
+};
+
+export default CallToAction;
