048e94dfa78bc3ec35cbf19f36215141a1ec80d0
Yusuf Alp <alpyusuf007@gmail.com>
2021-05-11 17:54:45 -0700

    add the button component

--- src/components/CallToAction/CallToAction.js
+++ src/components/CallToAction/CallToAction.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './CallToAction.css';
+import GetStartedButton from '../Button/GetStarted';
 
 const CallToAction = () => {
   return (
     <div className="call-to-action-main">
       <h2>Boost your links today</h2>
-      <button type="submit">Get Started</button>
+      <GetStartedButton />
     </div>
   );
 };
