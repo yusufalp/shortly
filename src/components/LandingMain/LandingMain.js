05d4a585578253134d65f30cc0efe2bd7d2ed761
Yusuf Alp <alpyusuf007@gmail.com>
2021-05-11 17:57:17 -0700

    add the button component

--- src/components/LandingMain/LandingMain.js
+++ src/components/LandingMain/LandingMain.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import './LandingMain.css';
+import GetStartedButton from '../Button/GetStarted';
+import LandingAction from '../LandingAction/LandingAction';
 
 const LandingMain = () => {
   return (
     <div className="landing-main">
       <div className="landing-group">
-        <div>
+        <div className="landing-title">
           <h1>More than just shorter links</h1>
           <p>
             Build your brand’s recognition and get detailed insights
             on how your links are performing.
-        </p>
-          <button type="submit">Get Started</button>
+          </p>
+          <GetStartedButton />
         </div>
         <div>
           <img className="landing-main-image" src="/images/illustration-working.svg" alt="person working on a desk" />
         </div>
       </div>
-      <div>
-        <input type="text" name="shorten" id="shorten" placeholder="Shorten a link here..." />
-        <button type="submit">Shorten it!</button>
-      </div>
+      <LandingAction />
     </div>
   );
 };
