97e9c6712cd23b2bcd29a43bfc1ba7af057593f1
Yusuf Alp <alpyusuf007@gmail.com>
2021-05-14 09:53:29 -0700

    add a button component

---
+++ src/components/Button/GetStarted.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './GetStarted.css';
+
+const GetStartedButton = () => {
+  return (
+    <button className="get-started-button" type="button">Get Started</button>
+  );
+};
+
+export default GetStartedButton;
