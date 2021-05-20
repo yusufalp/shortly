75584f580c056f62fc4e9e4cd33325f834ad39bb
Yusuf Alp <alpyusuf007@gmail.com>
2021-05-20 14:21:18 -0700

    import useHistory for route direction

--- src/components/Button/GetStarted.js
+++ src/components/Button/GetStarted.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './GetStarted.css';
+import { useHistory } from 'react-router-dom';
+
 
 const GetStartedButton = () => {
+
+  let history = useHistory();
+
+  const handleStart = () => {
+    history.push('/quiz');
+  };
+
   return (
-    <button className="get-started-button" type="button">Get Started</button>
+    <button className="get-started-button" type="button" onClick={handleStart}>Get Started</button>
   );
 };
 
